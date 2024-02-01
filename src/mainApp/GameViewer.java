75d94eb6c9b4f20b67c707977d0ec87ffb998599
mahoneip <mahoneip@137.112.201.137>
2024-02-01 11:05:49 -0500

    Global speed var

--- src/mainApp/GameViewer.java
+++ src/mainApp/GameViewer.java
@@ -19,7 +19,7 @@ public class GameViewer extends JPanel {
 
 
 	//
-
+	private static int speed=1;
 	public static final int WIDTH = 1280; // 
 	public static final int HEIGHT = 720; // 
 
@@ -88,7 +88,7 @@ public class GameViewer extends JPanel {
 			
 				handleGenerateObjects();
 				handleCheckGameOver();
-
+				speed++;
 				gameComponet.update();
 				gameComponet.repaint();
 			}
@@ -102,6 +102,9 @@ public class GameViewer extends JPanel {
 
 	public int getState() {
 		return state;
+	}
+	public static int getGameSpeed() {
+		return speed;
 	}
 
 	protected void handleGenerateObjects() {
