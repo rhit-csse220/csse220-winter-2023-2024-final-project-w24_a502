a4704c37fc0f9f5afe3fa525090052a73189a0f3
Haoyuan Fan <fanh@rose-hulman.edu>
2024-02-01 10:30:20 -0500

    change

--- src/mainApp/CollideableObject.java
+++ src/mainApp/CollideableObject.java
@@ -5,8 +5,13 @@ import java.awt.Graphics2D;
 public abstract class CollideableObject {
 
     public int x,y;
-    public double velX,velY,moveSpeed,angle;
+    public double velX,velY,angle;
+    public double moveSpeed;
     public boolean deathObject;
+
+
+
+    
 
     public abstract void drawOn(Graphics2D g2);
     public abstract void overlap();
