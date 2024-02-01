e01ba4b3bf0cf137aebaddc5fdf766dd5e8fa697
mahoneip <mahoneip@137.112.201.137>
2024-02-01 16:18:49 -0500

    implemets missle class methods update() constructor drawON()

--- src/mainApp/missile.java
+++ src/mainApp/missile.java
@@ -1,19 +1,35 @@
 package mainApp;
 
+import java.awt.Color;
 import java.awt.Graphics2D;
 import java.awt.geom.Dimension2D;
 
 public class missile extends CollideableObject {
 
-    public missile(int x2, int y2, int velX2, int velY2) {
-		super(x2, y2, velX2, velY2);
-		// TODO Auto-generated constructor stub
+    private static final double MISSLE_SPEED_MODIFER = 1/3;
+	private static final int MISSLE_SIZE = 30;
+	private static final int MISSLE_LAUNCH_SPEED = 100;
+	private int missleTimer=100;
+	public missile(int x2, int y2, int velX2, int velY2) {
+		super(GameViewer.WIDTH-MISSLE_SIZE, y2, 0, 0);
+		
 	}
 
 	@Override
     public void drawOn(Graphics2D g2) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'drawOn'");
+		g2.translate( x,y);
+		
+		if(missleTimer>0) {
+			g2.setColor(Color.YELLOW);
+			
+		}
+		else {
+			g2.setColor(Color.BLUE);
+		}
+		g2.fillRect(0, 0, MISSLE_SIZE, MISSLE_SIZE);
+		
+		g2.translate( -x,-y);
+       
     }
 
     @Override
@@ -24,8 +40,17 @@ public class missile extends CollideableObject {
 
     @Override
     public void update() {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'update'");
+    	
+    	if(missleTimer>0) {
+    		velY=((Player.getPosY()-this.y)*MISSLE_SPEED_MODIFER);
+    		missleTimer--;
+    	}
+    	else {
+    		velY=(0);
+    		velX=(-MISSLE_LAUNCH_SPEED);
+    		missleTimer=0;
+    	}
+    	super.update();
     }
     
 }
