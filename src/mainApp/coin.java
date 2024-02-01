7b7d970b9977ec1f90ddc3e5da6e5a2a6fb62a7e
mahoneip <mahoneip@137.112.201.137>
2024-02-01 10:35:07 -0500

    Added basics of coin drawing and partially fixed naming error

--- src/mainApp/coin.java
+++ src/mainApp/coin.java
@@ -1,13 +1,24 @@
 package mainApp;
 
 import java.awt.Graphics2D;
+import java.awt.geom.Ellipse2D;
 
-public class Coin extends CollideableObject {
-
+public class coin extends CollideableObject {
+	private int x;
+	private int y;
+	private int GLOBAL_SPEED;
+	private final static int COIN_RADIUS=30;
+	public coin(int x, int y) {
+		this.x=x;
+		this.y=y;
+	}
     @Override
     public void drawOn(Graphics2D g2) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'drawOn'");
+    	g2.translate(x, y);
+    	Ellipse2D coin = new Ellipse2D.Double(x,y, COIN_RADIUS,COIN_RADIUS);
+    	
+        g2.fill(coin);
+        g2.translate(-x,-y);
     }
 
     @Override
