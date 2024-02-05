5087603bf756616da286db074037be19a87bb158
mahoneip <mahoneip@137.112.201.70>
2024-02-05 14:09:19 -0500

    made Tracking missiles

---
+++ src/mainApp/TrackingMissile.java
@@ -0,0 +1,72 @@
+package mainApp;
+
+import java.awt.Color;
+import java.awt.Graphics2D;
+
+public class TrackingMissile extends missile {
+
+	public TrackingMissile(int x2) {
+		super(x2, Player.getPosY(), 0, 0);
+		// TODO Auto-generated constructor stub
+	}
+
+	private static final double MISSLE_SPEED_MODIFER = 0.9;
+	private int missleTimer=100;
+
+	@Override
+	public void update() {
+		
+    	if(missleTimer>0) {
+    		velX=-GameViewer.getGameSpeed();
+    		velY=((Player.getPosY()-this.y)*MISSLE_SPEED_MODIFER);
+    		missleTimer--;
+    	}
+    	else {
+    		velY=((Player.getPosY()-this.y));
+    		velX=((Player.getPosX()-this.x));
+    		double speedNorm=Math.sqrt(velX*velX+velY*velY);
+    		double gameSpeed= GameViewer.getGameSpeed();
+    		velY=-gameSpeed*velY/speedNorm*MISSLE_SPEED_MODIFER;
+    		velX=-gameSpeed*velX/speedNorm*MISSLE_SPEED_MODIFER;
+    		missleTimer=0;
+    	}
+    	this.x+=GameViewer.getGameSpeed()+velX;
+    	
+    	this.y+=velY;
+    	
+    	if(y<=GameViewer.getCeiling()) {
+    		velY=0;
+    		y=GameViewer.getCeiling();
+    	}
+    	if(y>=GameViewer.getFloor()) {
+    		y=GameViewer.getFloor();
+    		velY=0;
+    	}
+    	
+	}
+
+	@Override
+  public void drawOn(Graphics2D g2) {
+		
+		g2.translate( x,y);
+		
+		if(missleTimer>0) {
+			g2.setColor(Color.YELLOW);
+			
+		}
+		else {
+			g2.setColor(Color.BLUE);
+		}
+		g2.fillRect(0, 0, MISSLE_SIZE, MISSLE_SIZE);
+		
+		g2.translate( -x,-y);
+     
+  }
+
+	@Override
+	public void overlap() {
+		// TODO Auto-generated method stub
+
+	}
+
+}
